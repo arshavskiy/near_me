9051c1b3fab5abe1e52871c826dc27f573ed3fec
pavel <arshavsky.pasha@gmail.com>
2019-06-21 08:12:19 +0300

    teting search

--- scripts/main.js
+++ scripts/main.js
@@ -54,6 +54,16 @@ let initLeafMap = () => {
     
     L.map.onload = setMarkersOnMapLoad();
 
+    let searchControl = new L.esri.Controls.Geosearch().addTo(map);
+    let results = new L.LayerGroup().addTo(map);
+  
+    searchControl.on('results', function(data){
+      results.clearLayers();
+      for (let i = data.results.length - 1; i >= 0; i--) {
+        results.addLayer(L.marker(data.results[i].latlng));
+      }
+    });
+
     
 
      
