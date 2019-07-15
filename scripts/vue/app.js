8db7e02ac04ca5ac027471e84adb0533ab38cc1b
pavel <arshavsky.pasha@gmail.com>
2019-07-15 22:04:05 +0300

    smal fixes

--- scripts/vue/app.js
+++ scripts/vue/app.js
@@ -57,15 +57,16 @@ let app = new Vue({
         gsradius: 1000,
         selected: cardTitle => localStorage.getItem(cardTitle),
         toggleMenuOpen: false,
-        maps: [{
+        maps: [
+            {
             name: 'Map',
             value: () => {
                 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}')
                     .addTo(mymap);
-            }
-        },
+                }
+            },
             {
-                name: 'Map',
+                name: 'Simple',
                 value: () => {
                     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                         maxZoom: 19,
